c2eda68920b0b3ed21678508e28d026fb0fe2c1f
Juliana Connors <juliana.g.connors@gmail.com>
2020-07-20 18:56:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,27 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS.collect do |new_brand|
+      BRANDS.!include?(new_brand)
+      BRANDS << new_brand
+    end  
   end
-
+  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
+  # def unique_brands(brand)
+  #   if BRANDS.include?(brand)
+  #     BRANDS.pop
+  #     return BRANDS
+  #   end
+  # end
 
 end
